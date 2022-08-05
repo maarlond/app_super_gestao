c23f01c8e5514fcf65f272196eaf1d1cd3ec8c4e
Marlon Dietrich <marlon-dietrich@SEPLAG.INTRA.RS.GOV.BR>
2022-08-05 16:07:25 -0300

    6 - Agrupando rotas

--- routes/web.php
+++ routes/web.php
@@ -29,17 +29,21 @@ Route::get('/login', function () {
     return 'Login';
 });
 
-Route::get('/clientes', function () {
-    return 'Clientes';
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    });
+
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    });
+
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    });
 });
 
-Route::get('/fornecedores', function () {
-    return 'Fornecedores';
-});
-
-Route::get('/produtos', function () {
-    return 'Produtos';
-});
 
 
 
