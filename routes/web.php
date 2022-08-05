8136c3fec0c0460ba3cd610114668cedd72079a8
Marlon Dietrich <marlon-dietrich@SEPLAG.INTRA.RS.GOV.BR>
2022-08-05 16:15:22 -0300

    7 - Nomeando rotas

--- routes/web.php
+++ routes/web.php
@@ -18,30 +18,30 @@ Route::get('/', function () {
 });
 */
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 // nome, categoria, assunto, mensagem
 
 Route::get('/login', function () {
     return 'Login';
-});
+})->name('site.login');
 
 
 Route::prefix('/app')->group(function () {
     Route::get('/clientes', function () {
         return 'Clientes';
-    });
+    })->name('app.clientes');
 
     Route::get('/fornecedores', function () {
         return 'Fornecedores';
-    });
+    })->name('app.fornecedores');
 
     Route::get('/produtos', function () {
         return 'Produtos';
-    });
+    })->name('app.produtos');
 });
 
 
