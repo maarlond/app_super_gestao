8efff6bd07e1718fd7077fe17c25b0da48ac325b
Marlon Dietrich <marlon-dietrich@SEPLAG.INTRA.RS.GOV.BR>
2022-08-05 14:50:35 -0300

    Paramêtros opcionais e valores padrões

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,8 @@ Route::get('/contato', 'ContatoController@contato');
 
 
 Route::get(
-    '/contato/{nome}/{categoria}/{assunto}/{mensagem}',
-    function (string $nome, string $categoria, string $assunto, string $mensagem) {
+    '/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
+    function (string $nome = 'Desconhecido', string $categoria = 'Informação', string $assunto = 'Contato', string $mensagem = 'mensagem não informada') {
         echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
     }
 );
