e70b0ae07bdd97bfc7abeec83ec9e7f5b3f5b008
Marlon Dietrich <marlon-dietrich@SEPLAG.INTRA.RS.GOV.BR>
2022-08-05 15:51:32 -0300

    4 - Criando menu de navegação

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,9 @@ Route::get('/contato', 'ContatoController@contato');
 // nome, categoria, assunto, mensagem
 
 
-Route::get(
-    '/contato/{nome}/{categoria_id}',
-    function (string $nome = 'Desconhecido', int $categoria_id = 1) { // 1 - 'Informação'
-        echo "Estamos aqui: $nome - $categoria_id";
-    }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+// Route::get(
+//     '/contato/{nome}/{categoria_id}',
+//     function (string $nome = 'Desconhecido', int $categoria_id = 1) { // 1 - 'Informação'
+//         echo "Estamos aqui: $nome - $categoria_id";
+//     }
+// )->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
