a9a0faa0ca48f2b11b9c1b434b2209e6c784b67f
Marlon Dietrich <marlon-dietrich@SEPLAG.INTRA.RS.GOV.BR>
2022-08-09 15:57:06 -0300

    12 - Incluindo comentários e blocos php puros blade

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,14 @@
+<h3>Fornecedor</h3>
+
+{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
+
+{{ 'Texto de teste' }} <br>
+<?= 'Texto de teste' ?>
+@php
+// Para comentários de uma linha
+/*
+Para comentários de múltiplas linhas
+*/
+
+echo 'Texto de teste';
+@endphp
--- routes/web.php
+++ routes/web.php
@@ -35,9 +35,7 @@ Route::prefix('/app')->group(function () {
         return 'Clientes';
     })->name('app.clientes');
 
-    Route::get('/fornecedores', function () {
-        return 'Fornecedores';
-    })->name('app.fornecedores');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
 
     Route::get('/produtos', function () {
         return 'Produtos';
