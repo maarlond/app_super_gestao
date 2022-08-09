42e0b8b6f45b3fe97b0f537d1a135c6f92448abd
Marlon Dietrich <marlon-dietrich@SEPLAG.INTRA.RS.GOV.BR>
2022-08-09 15:10:05 -0300

    Encaminhando parâmetros da rota para o controlador

--- routes/web.php
+++ routes/web.php
@@ -44,15 +44,8 @@ Route::prefix('/app')->group(function () {
     })->name('app.produtos');
 });
 
-Route::get('/rota1', function () {
-    echo 'Rota 1';
-})->name('site.rota1');
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
 
-Route::get('/rota2', function () {
-    return redirect()->route('site.rota1');
-})->name('site.rota2');
-
-// Route::redirect('/rota2', '/rota1');
 
 Route::fallback(function () {
     echo 'A rota acessada não existe, <a href="' . route('site.index') . '"> clique aqui </a> para ir para a página inicial';
