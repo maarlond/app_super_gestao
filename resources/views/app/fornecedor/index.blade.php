995ffdf1842c7bdd37ae6582a35d971d88aec2d6
Marlon Dietrich <marlon-dietrich@SEPLAG.INTRA.RS.GOV.BR>
2022-08-11 10:08:21 -0300

    15 - Blad - @isset

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -32,3 +32,13 @@ Status: {{ $fornecedores[0]['status'] }}
     <!-- se o retorno da condição for false -->
     Fornecedor Inativo
 @endunless
+
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}
+    @endisset
+@endisset
